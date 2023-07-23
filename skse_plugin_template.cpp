be8ab59dfdde460680dbd48dee71686923cede00
Michael Yarichuk <michael.yarichuk@gmail.com>
2023-07-23 18:16:56 +0300

    chore: initial basic commit

---
+++ skse_plugin_template.cpp
@@ -0,0 +1,12 @@
+﻿// skse_plugin_template.cpp : Defines the entry point for the application.
+//
+
+#include "skse_plugin_template.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
