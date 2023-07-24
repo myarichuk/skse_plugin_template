fc38997c013572f4f30349e3f80f06b547c9dfd4
Michael Yarichuk <michael.yarichuk@gmail.com>
2023-07-25 01:37:03 +0300

    chore: remove pyro from the build -> needs refactoring

--- tools/EspGenerator/Program.cs
+++ tools/EspGenerator/Program.cs
@@ -1,12 +1,10 @@
-﻿using Mutagen.Bethesda;
+﻿using System.IO.Abstractions;
+using Mutagen.Bethesda.Plugins;
+using Mutagen.Bethesda.Plugins.Binary.Parameters;
 using Mutagen.Bethesda.Skyrim;
-using Mutagen.Bethesda.Synthesis;
 
-await SynthesisPipeline.Instance
-    .AddPatch<ISkyrimMod, ISkyrimModGetter>(RunPatch)
-    .SetTypicalOpen(GameRelease.SkyrimSE, "YourPatcher.esp")
-    .Run(args);
-
-static void RunPatch(IPatcherState<ISkyrimMod, ISkyrimModGetter> state)
+var mod = new SkyrimMod(ModKey.FromFileName("skse_plugin_template.esp"), SkyrimRelease.SkyrimSE);
+mod.WriteToBinary("./skse_plugin_template.esp", fileSystem: new FileSystem(), param: new BinaryWriteParameters()
 {
-}
+    ModKey = ModKeyOption.NoCheck
+});
